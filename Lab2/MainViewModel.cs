5e4d0b805ca700ffc3e7d18309b836eb2a96fa82
Mariia Shpir <99517424+shpirm@users.noreply.github.com>
2024-03-16 03:24:46 +0200

    commit lab2

---
+++ Lab2/MainViewModel.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfApp.Model;
+
+namespace WpfApp.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Person _person;
+        private bool _isUIEnabled = true;
+
+        public MainViewModel()
+        {
+            _person = new Person();
+            ProceedCommand = new RelayCommand(async () => await ProceedAsync());
+            PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName != nameof(IsValid))
+                {
+                    OnPropertyChanged(nameof(IsValid));
+                }
+            };
+        }
+
+        public string FirstName
+        {
+            get => _person.FirstName;
+            set
+            {
+                _person.FirstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get => _person.LastName;
+            set
+            {
+                _person.LastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Email
+        {
+            get => _person.Email;
+            set
+            {
+                _person.Email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime BirthDate
+        {
+            get => _person.BirthDate;
+            set
+            {
+                _person.BirthDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsValid => !string.IsNullOrEmpty(_person.FirstName)
+                            && !string.IsNullOrEmpty(_person.LastName)
+                            && !string.IsNullOrEmpty(_person.Email)
+                            && BirthDate != DateTime.MinValue;
+
+        public bool IsUIEnabled
+        {
+            get => _isUIEnabled;
+            set
+            {
+                _isUIEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ProceedCommand { get; }
+
+        private async Task ProceedAsync()
+        {
+            IsUIEnabled = false;
+
+            await Task.Run(() =>
+            {
+                if (!_person.IsAgeValid)
+                {
+                    MessageBox.Show("Invalid age. The age should be between 0 and 135 years.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                string info = $"First Name: {_person.FirstName}\n" +
+                              $"Last Name: {_person.LastName}\n" +
+                              $"Email: {_person.Email}\n" +
+                              $"Birth Date: {_person.BirthDate.ToShortDateString()}\n" +
+                              $"Age: {_person.Age}\n" +
+                              $"Western Zodiac: {_person.WesternZodiac}\n" +
+                              $"Chinese Zodiac: {_person.ChineseZodiac}\n" +
+                              $"Is Adult: {_person.IsAdult}\n" +
+                              $"Is Birthday: {_person.IsBirthday}";
+
+                MessageBox.Show(info, "User Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            });
+
+            IsUIEnabled = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Lab2/Person.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace WpfApp.Model
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public int Age => CalculateAge();
+        public string WesternZodiac => GetWesternZodiac(BirthDate);
+        public string ChineseZodiac => GetChineseZodiac(BirthDate.Year);
+        public bool IsAdult => Age >= 18;
+        public bool IsBirthday => BirthDate.Month == DateTime.Today.Month
+                                   && BirthDate.Day == DateTime.Today.Day;
+        public bool IsAgeValid => Age >= 0 && Age <= 135;
+
+        public Person()
+        {
+            // Default values
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Email = string.Empty;
+            BirthDate = DateTime.MinValue;
+        }
+
+        public Person(string firstName, string lastName, string email, DateTime birthDate)
+            : this()
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public Person(string firstName, string lastName, string email)
+            : this(firstName, lastName, email, DateTime.MinValue)
+        {
+        }
+
+        public Person(string firstName, string lastName, DateTime birthDate)
+            : this(firstName, lastName, string.Empty, birthDate)
+        {
+        }
+
+        private int CalculateAge()
+        {
+            var today = DateTime.Today;
+            var age = today.Year - BirthDate.Year;
+            if (BirthDate.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+
+        private string GetWesternZodiac(DateTime birthDate)
+        {
+            int[] zodiacStartDates = { 20, 19, 21, 20, 21, 21, 23, 23, 23, 23, 22, 22 };
+            string[] zodiacSigns = {
+                "Aquarius", "Pisces", "Aries", "Taurus",
+                "Gemini", "Cancer", "Leo", "Virgo",
+                "Libra", "Scorpio", "Sagittarius", "Capricorn" };
+
+            int index = birthDate.Month - 1;
+            if (birthDate.Day < zodiacStartDates[index])
+            {
+                index = (index + 11) % 12;
+            }
+
+            return zodiacSigns[index];
+        }
+
+        private string GetChineseZodiac(int year)
+        {
+            string[] zodiacSigns = {
+                "Rat", "Ox", "Tiger", "Rabbit",
+                "Dragon", "Snake", "Horse", "Goat",
+                "Monkey", "Rooster", "Dog", "Pig" };
+
+            int index = (year - 4) % 12;
+            if (index < 0)
+            {
+                index += 12;
+            }
+            return zodiacSigns[index % 12];
+        }
+    }
+}
